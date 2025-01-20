41c636fe18d7c8702843f49887af6e2cb684d8df
LordOfTheCorgis <128561696+LordOfTheCorgis@users.noreply.github.com>
2025-01-19 21:15:00 -0600

    Update fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -5,8 +5,13 @@ description 'Weapon Jam Mechanic'
 
 author 'Simplify | Corgis Customs'
 version '1.0.0'
+lua54 'yes'
 
 client_scripts {
     'config.lua',
     'client/main.lua'
+}
+
+shared_scripts {
+    '@ox_lib/init.lua',
 }
